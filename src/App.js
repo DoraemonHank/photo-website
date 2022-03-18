03862d09fa5547398ed4e80e03937cf45ce05f20
DoraemonHank <tsj10226@gmail.com>
2022-03-19 01:59:12 +0800

    ddd

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Nav/>
       <Switch>
-        <Route path = "/" exact> 
+        <Route path = "/photo-website" exact> 
           <Homepage/>
         </Route>
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <nav>
         <ul>
-            <li><Link to="/">Home</Link></li>
+            <li><Link to="/photo-website">Home</Link></li>
             <li><Link to="/about">About</Link></li>
         </ul>
     </nav>
